cc4eea855eb8dc64280985c3b5dd81ab8af57ace
DamarisEspinosa <2130081@upv.edu.mx>
2024-03-18 00:22:12 -0600

    new style

--- logicGame.js
+++ logicGame.js
@@ -1,17 +1,18 @@
 window.onload = function() {
     var parametrosURL = new URLSearchParams(window.location.search);
-    var tamaño = parametrosURL.get('tamanioTablero');
+    var filas = parametrosURL.get('cantFilas');
+    var columnas = parametrosURL.get('cantColumnas');
 
-    crearTablero(tamaño);
+    crearTablero(filas, columnas);
 };
 
-function crearTablero(tamaño) {
+function crearTablero(filas, columnas) {
     var contenedor = document.getElementById("contenedor-tablero");
 
-    for (var i = 0; i < tamaño; i++) {
+    for (var i = 0; i < filas; i++) {
         var fila = document.createElement("div");
         fila.className = "fila";
-        for (var j = 0; j < tamaño; j++) {
+        for (var j = 0; j < columnas; j++) {
             var cuadro = document.createElement("div");
             cuadro.className = "cuadro";
             fila.appendChild(cuadro);
